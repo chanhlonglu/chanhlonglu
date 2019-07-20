1ec3109631ab4c20c4f986072234ff6b8394f298
Chanh Long Lu <chanh.lu@intergen.co.nz>
2019-07-20 17:06:33 +1200

    Add header and setup grid layout.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from "react";
-import { Switch, Route, NavLink } from "react-router-dom";
+import { Switch, Route, Link, NavLink } from "react-router-dom";
 import Home from "./components/Home";
 import Experience from "./components/Experience";
 import NotFound from "./components/NotFound";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <div>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/experience">Experience</NavLink>
+      <div className="header">
+        <div className="header-logo">
+          <h1>
+            <Link to="/">Chanh Long Lu</Link>
+          </h1>
+        </div>
+        <div className="header-menu">
+          <NavLink exact to="/" activeClassName="header-menu-selected">
+            Home
+          </NavLink>
+          <NavLink to="/experience" activeClassName="header-menu-selected">
+            Experience
+          </NavLink>
+          <NavLink to="/eduction" activeClassName="header-menu-selected">
+            Education
+          </NavLink>
+        </div>
       </div>
       <Switch>
         <Route exact path="/" component={Home} />
