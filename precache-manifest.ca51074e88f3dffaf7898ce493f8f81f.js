85f7e1d49f704c7f86444c35fc185a672254e83e
Chanh Long Lu <chanh.lu@intergen.co.nz>
2019-07-20 15:50:18 +1200

    Updates

---
+++ precache-manifest.ca51074e88f3dffaf7898ce493f8f81f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cce0b6e74dba2b55ba3496af99cdcd1",
+    "url": "/chanhlonglu/index.html"
+  },
+  {
+    "revision": "9033638c3457b0036cb0",
+    "url": "/chanhlonglu/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/chanhlonglu/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "9033638c3457b0036cb0",
+    "url": "/chanhlonglu/static/js/main.deef8bf1.chunk.js"
+  },
+  {
+    "revision": "c80650989b6247e4b21d",
+    "url": "/chanhlonglu/static/js/runtime~main.b1c22c26.js"
+  }
+]);
