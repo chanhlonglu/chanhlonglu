23d276ef72bbae556bf576a303aae0d9c2b88d5e
Chanh Long Lu <chanh.lu@intergen.co.nz>
2019-07-21 21:49:15 +1200

    Updates

---
+++ precache-manifest.5f032fe274bcbc4293d9307d01a02da4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093bcec50bd58361bcb0f895aaa30606",
+    "url": "/chanhlonglu/index.html"
+  },
+  {
+    "revision": "9dd2fff7e0c3899fb631",
+    "url": "/chanhlonglu/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "efc9ea30f68e1ebaa6a8",
+    "url": "/chanhlonglu/static/css/main.9d8f7f46.chunk.css"
+  },
+  {
+    "revision": "9dd2fff7e0c3899fb631",
+    "url": "/chanhlonglu/static/js/2.be09c6b4.chunk.js"
+  },
+  {
+    "revision": "efc9ea30f68e1ebaa6a8",
+    "url": "/chanhlonglu/static/js/main.ccaa325d.chunk.js"
+  },
+  {
+    "revision": "c80650989b6247e4b21d",
+    "url": "/chanhlonglu/static/js/runtime~main.b1c22c26.js"
+  },
+  {
+    "revision": "4fe717c5a36493f997f045938ca0ce80",
+    "url": "/chanhlonglu/static/media/intergen.4fe717c5.png"
+  },
+  {
+    "revision": "42b5907f6bbc9a22c09792cac3c4af15",
+    "url": "/chanhlonglu/static/media/lazyworm-application.42b5907f.png"
+  },
+  {
+    "revision": "132aafd6dd909ba8739e1a1b90bf8411",
+    "url": "/chanhlonglu/static/media/microsoft-nz.132aafd6.png"
+  },
+  {
+    "revision": "3e4f3a5ee951932d13058f6bcc484fa6",
+    "url": "/chanhlonglu/static/media/university-of-auckland.3e4f3a5e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1cb5318a810889a96d1344a8bf68f354.js"
+  "/chanhlonglu/precache-manifest.5f032fe274bcbc4293d9307d01a02da4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chanhlonglu/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
